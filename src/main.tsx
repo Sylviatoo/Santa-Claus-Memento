4ee799c6e15ad0431b2ed807d21ea7cbba3bf252
Sylvia Barraud <163527980+Sylviatoo@users.noreply.github.com>
2024-12-26 21:06:54 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import App from "./App.tsx";
-import SecondPage from "./Pages/SecondPage.tsx";
-import WelcomePage from "./Pages/WelcomePage.tsx";
+import App from "./App";
+import SecondPage from "./Pages/SecondPage";
+import WelcomePage from "./Pages/WelcomePage";
 
 const router = createBrowserRouter([
   {
